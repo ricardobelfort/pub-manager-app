785343afa2234633d22be3fad03e8f673e4de30b
Ricardo Belfort <rbelfort2004@gmail.com>
2026-01-10 15:13:07 -0400

    primeiro commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ZardButtonComponent } from './shared/components/button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ZardButtonComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('pub-manager-app');
